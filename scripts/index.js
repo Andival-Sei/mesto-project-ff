ca7b61aedc684144ee48585b76b15323779fb941
Kirill Barkov <freedomdragon777@gmail.com>
2025-04-13 01:01:40 +0400

    Добавлены функции для создания, удаления и отображения карточек на странице. Реализован механизм клонирования шаблона карточки и добавления обработчиков событий.

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,48 @@
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// Получаем шаблон карточки из HTML
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция удаления карточки
+// Получаем контейнер для карточек
+const placesList = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+// Функция создания карточки
+function createCard(cardData, deleteCard) {
+  // Клонируем шаблон карточки
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  // Находим элементы внутри карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  // Устанавливаем значения из данных карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  
+  // Добавляем обработчик удаления карточки
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+  
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Функция отображения карточек на странице
+function renderCards() {
+  // Перебираем массив карточек и добавляем каждую на страницу
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+// Вызываем функцию отображения карточек при загрузке страницы
+renderCards();
